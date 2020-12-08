c053522ac57dd2eb59f45724056bfd4cd6a5bc8b
learnforpractice <learnforpractice@gmail.com>
2020-12-08 23:08:39 +0800

    Fix execution with native uuoskit

--- test_helloworld/test.py
+++ test_helloworld/test.py
@@ -1,25 +1,24 @@
-from browser import aio, window
 from uuoskit import chainapi, wallet
-import ujson as json
+from uuoskit import test_helper
 
-import test_helper
+test_helper.config_network()
 src, abi = test_helper.load_code()
 
 async def test():
     wallet.create('test')
     wallet.import_key('test', '5JRYimgLBrRLCBAcjHUWCYRv3asNedTYYzVgmiU4q2ZVxMBiJXL')
-    uuosapi = chainapi.ChainApi('http://127.0.0.1:8888')
-    code = uuosapi.compile('hello', src)
+    uuosapi = chainapi.ChainApiAsync('http://127.0.0.1:8888')
+    code = await uuosapi.compile('hello', src, vm_type=1)
 
     try:
         r = await uuosapi.deploy_contract('hello', code, abi, vmtype=1)
     except chainapi.ChainException as e:
-        print('+++deploy error:', e.error.message)
+        print('+++deploy error:', e)
 
     args = b'hello,world'.hex()
     try:
         r = await uuosapi.push_action('hello', 'sayhello', args, {'hello': 'active'})
-        print(r.processed.action_traces[0].console)
+        print(r['processed']['action_traces'][0]['console'])
     except chainapi.ChainException as e:
         print(e)
 
@@ -29,4 +28,4 @@ async def run_test():
     except Exception as e:
         print(e)
 
-aio.run(run_test())
+test_helper.run(run_test())
