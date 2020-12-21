f26c0a458514b433e37d849d7f2fd1b4b0638878
learnforpractice <learnforpractice@gmail.com>
2020-12-21 12:50:02 +0800

    Update hash example

--- test_hash/code.py
+++ test_hash/code.py
@@ -2,14 +2,18 @@ import chain
 def apply(receiver, first_receiver, action):
     data = 'hello,world'
     h = chain.sha256(data)
+    print('hash256:', h)
     chain.assert_sha256(data, h)
 
     h = chain.sha1(data)
+    print('sha1:', h)
     chain.assert_sha1(data, h)
 
     h = chain.sha512(data)
+    print('sha512', h)
     chain.assert_sha512(data, h)
 
     h = chain.ripemd160(data)
+    print('ripemd160:', h)
     chain.assert_ripemd160(data, h)
     print('all tests passed!')
