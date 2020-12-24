e853cae8ad47ec53cc5c3ad5af81c5a764c6758d
learnforpractice <learnforpractice@gmail.com>
2020-12-24 20:04:38 +0800

    Update inline action test

--- test_inline_action/test.py
+++ test_inline_action/test.py
@@ -24,13 +24,7 @@ async def update_code_auth(uuosapi):
 
 src, abi = test_helper.load_code()
 
-# Replace the following default test account and test private key
-# with your own. Because someone may use the same test account at the same time,
-# that will cause conflict. If you don't have a test account,
-# go to https://testnet.eos.io and get one.
-test_account1 = 'wkpmdjdsztyu'
-wallet.import_key('test', '5Jaz37nnxbpAiAGQEsyxtnGfCPTJFjX9Wn6zv7V41Ko6DXSqhd9')
-
+test_account1 = test_helper.test_account1
 
 async def run_test():
     uuosapi = chainapi.ChainApiAsync(config.network_url)
@@ -46,14 +40,10 @@ async def run_test():
     except chainapi.ChainException as e:
         print('+++deploy error:', e.error.message)
 
-    args = 'hello,world'
-    r = await uuosapi.push_action(test_account1, 'testinline', args, {test_account1: 'active'})
+    args = b'hello,world'
+    r = await uuosapi.push_action(config.python_contract, 'testinline', args, {test_account1: 'active'})
     print('+++testinline:', r['processed']['action_traces'][0]['console'])
     print('+++sayhello:', r['processed']['action_traces'][0]['inline_traces'][0]['console'])
     print('+++++++push_action elapsed:', r['processed']['elapsed'])
-
-
-
-
 
 
