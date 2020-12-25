7883178a8008555983e5368762fcf359b35cb6ee
learnforpractice <learnforpractice@gmail.com>
2020-12-25 22:15:59 +0800

    Fix authorization test

--- test_authorization/test.py
+++ test_authorization/test.py
@@ -4,17 +4,10 @@ from uuoskit import test_helper
 
 src, abi = test_helper.load_code()
 
-# Replace the following default test account and test private key
-# with your own. Because someone may use the same test account at the same time,
-# that will cause conflict. If you don't have a test account,
-# go to https://testnet.eos.io and get one.
 if config.network == 'EOS_TESTNET':
-    test_account1 = 'wkpmdjdsztyu'
-    wallet.import_key('test', '5Jaz37nnxbpAiAGQEsyxtnGfCPTJFjX9Wn6zv7V41Ko6DXSqhd9')
+    test_account1 = test_helper.test_account1
 else:
-    test_account1 = 'helloworld11'
-
-
+    test_account1 = 'hello'
 
 async def run_test():
     uuosapi = chainapi.ChainApiAsync(config.network_url)
@@ -22,14 +15,14 @@ async def run_test():
 
     r = await uuosapi.deploy_contract(test_account1, code, abi, vm_type=1)
 
-    args = 'hello,world'
+    args = b'hello,world'
     try:
         r = await uuosapi.push_action(test_account1, 'test1', args, {test_account1: 'active'})
         test_helper.print_console(r)
     except chainapi.ChainException as e:
         print('+++test1 error:', e)
 
-    args = 'hello,world'
+    args = b'hello,world'
     try:
         r = await uuosapi.push_action(test_account1, 'test2', args, {test_account1: 'active'})
         test_helper.print_console(r)
@@ -37,8 +30,4 @@ async def run_test():
         msg = e.error.message
         print('++++test2 error:', msg)
         assert msg == f"missing authority of {test_account1}/owner"
-
-
-
-
 
