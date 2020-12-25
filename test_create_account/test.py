52a0b4c332e80d588c93a55f348737b5200b8cdf
learnforpractice <learnforpractice@gmail.com>
2020-12-25 22:29:30 +0800

    Update create account example

--- test_create_account/test.py
+++ test_create_account/test.py
@@ -25,21 +25,14 @@ async def update_code_auth(uuosapi):
 
 src, abi = test_helper.load_code()
 
-# Replace the following default test account and test private key
-# with your own. Because someone may use the same test account at the same time,
-# that will cause conflict. If you don't have a test account,
-# go to https://testnet.eos.io and get one.
-if config.network == 'EOS_TESTNET':
-    test_account1 = 'wkpmdjdsztyu'
-    wallet.import_key('test', '5Jaz37nnxbpAiAGQEsyxtnGfCPTJFjX9Wn6zv7V41Ko6DXSqhd9')
-else:
-    test_account1 = 'helloworld11'
 
-
+test_account1 = test_helper.test_account1
 test_account2 = test_helper.test_account2
-#config.contract_deploy_type = 1
 
 async def run_test():
+    if config.network == 'EOS_TESTNET':
+        print('This example not work no EOS Testnet')
+        return
     uuosapi = chainapi.ChainApiAsync(config.network_url)
     code = await uuosapi.compile(test_account1, src, vm_type=1)
     await update_code_auth(uuosapi)
@@ -51,8 +44,3 @@ async def run_test():
     r = await uuosapi.transfer(test_account2, test_account1, 1.0, memo)
     console = r['processed']['action_traces'][0]['inline_traces'][1]['console']
     print(console)
-
-
-
-
-
